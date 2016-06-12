95a7e85b8a9dacea84ea31e6e5fe53f0f4ea1fba
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2016-06-12 17:11:46 -0500

    Switched search form from POST to GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   
   get 'users/:id/vcard', to: 'users#vcard', as: 'user_vcard'
-  post 'users', to: 'users#search', as: 'users_search'
+  get 'users', to: 'users#search', as: 'users_search'
   get 'users/map/(:region)', to: 'users#map', as: 'users_map'
   resources :users, except: :create
 
