9a1771b5aeb90372bf7cb41dc0e9c59488250b4a
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2016-06-17 18:30:00 -0500

    Allow to updated profile picture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -156,7 +156,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :first_year, :facebook, :linkedin, 
+    params.require(:user).permit(:last_name, :first_name, :first_year, :facebook, :linkedin, :avatar, 
        personal_contacts_attributes: [:id, :address_1, :address_2, :address_3, :country, :state, :zip_code, :city, :extra, :phone, :mobile, :email],
        profesional_contacts_attributes: [:id, :address_1, :address_2, :address_3, :country, :state, :zip_code, :city, :extra, :phone, :mobile, :email, :company, :job, :company_website]
     )
