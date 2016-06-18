d247b4cd5db8cb6843b2911ac2a9b87653296a69
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2016-06-18 17:54:45 -0500

    Fixes for production

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,10 +17,10 @@ module UsersHelper
   def linkedin_tag(user)
     if user.linkedin.present?
       link_to linkedin_url(user), target: '_blank' do
-        image_tag 'logo_linkedin', size: '25x25'
+        image_tag 'logo_linkedin.png', size: '25x25'
       end
     else
-      image_tag 'logo_linkedin', size: '25x25', class: 'disabled'
+      image_tag 'logo_linkedin.png', size: '25x25', class: 'disabled'
     end
   end
 
@@ -31,28 +31,28 @@ module UsersHelper
   def facebook_tag(user)
     if user.facebook.present?
       link_to facebook_url(user), target: '_blank' do
-        image_tag 'logo_facebook', size: '25x25'
+        image_tag 'logo_facebook.png', size: '25x25'
       end
     else
-      image_tag 'logo_facebook', size: '25x25', class: 'disabled'
+      image_tag 'logo_facebook.png', size: '25x25', class: 'disabled'
     end
   end
 
   def uniandes_tag(user)
     mail_to user.email do
-      image_tag 'logo_uniandes', size: '25x25'
+      image_tag 'logo_uniandes.png', size: '25x25'
     end
   end
 
   def vcard_tag(user)
     link_to user_vcard_path(user), target: '_blank' do
-      image_tag 'vcf'
+      image_tag 'vcf.gif'
     end
   end
 
   def cv_tag(user)
     link_to user.cv.url, target: '_blank' do
-      image_tag 'cv'
+      image_tag 'cv.gif'
     end
   end
 end
